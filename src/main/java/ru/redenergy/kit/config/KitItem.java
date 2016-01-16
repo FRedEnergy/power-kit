b08939a2133ff46f5fd138c74390dda16fb75c99
RedEnergy <forwot163@gmail.com>
2016-01-16 21:43:51 +0400

    Added method to create itemstack from KitItem

--- src/main/java/ru/redenergy/kit/config/KitItem.java
+++ src/main/java/ru/redenergy/kit/config/KitItem.java
@@ -1,5 +1,8 @@
 package ru.redenergy.kit.config;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 
@@ -34,5 +37,13 @@ public class KitItem {
 
     public NBTBase getNBT() {
         return nbt;
+    }
+
+    public ItemStack createItemStack(){
+        String[] identifier = item.split(":");
+        Item item = GameRegistry.findItem(identifier[0], identifier[1]);
+        ItemStack stack = new ItemStack(item, amount, metadata);
+        stack.stackTagCompound = (NBTTagCompound) nbt;
+        return stack;
     }
 }
